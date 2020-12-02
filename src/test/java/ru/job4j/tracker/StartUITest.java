a98e203499e31d9074c1a9596a0f70068ae10ea4
Artem <artem.polschak@gmail.com>
2020-12-02 14:37:02 +0200

    9.2. Тесты вывода на консоль [#367580]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,8 +2,6 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import static org.hamcrest.core.IsNull.nullValue;
