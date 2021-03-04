df90b2f8f36d4f45e31b30d085683d3c99c60b78
lob4j <artem.polschak@gmail.com>
2021-03-04 16:55:52 +0200

    0. Коллекции, ArrayList, List, Обобщения [#367566]

--- src/main/java/ru/job4j/collection/UserArrayList.java
+++ src/main/java/ru/job4j/collection/UserArrayList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class UserArrayList {
     public static void main(String[] args) {
-        //array
+
         ArrayList<String> list = new ArrayList<>();
         list.add("Petr");
         list.add("Ivan");
