d141fcdbe71071f4cbc5c74620327c245dd63c5a
Artem <artem.polschak@gmail.com>
2021-04-16 21:51:44 +0300

    1. Паспорт и жители [#367569]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arstntev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+
+}
