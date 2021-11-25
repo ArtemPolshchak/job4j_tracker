bd3fc7251101e65bc20699a6e788b039407195c1
Artem <56567708+ArtemPolshchak@users.noreply.github.com>
2021-11-25 21:41:57 +0200

    1.4. Создание стрима. Метод Stream.iterate()

---
+++ src/main/java/ru/job4j/stream/StreamIterate.java
@@ -0,0 +1,11 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+public class StreamIterate {
+    public static void showArray(Integer[] data) {
+        Stream.iterate(0, i -> i < data.length - 1, i -> i + 2)
+                .forEach(i -> System.out.println(data[i]));
+        }
+    }
+
