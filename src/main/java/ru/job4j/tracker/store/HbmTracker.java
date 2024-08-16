03fc69b2d7701c4454952102e03e42a7fedad8f8
Artem Polshchak <artem@example.com>
2024-08-16 16:03:22 +0100

    3. HbmTracker [#287705]

--- src/main/java/ru/job4j/tracker/store/HbmTracker.java
+++ src/main/java/ru/job4j/tracker/store/HbmTracker.java
@@ -1,7 +1,9 @@
 package ru.job4j.tracker.store;
 
+import org.hibernate.Session;
 import ru.job4j.tracker.model.Item;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
@@ -17,31 +19,105 @@ public class HbmTracker implements Store {
 
     @Override
     public Item add(Item item) {
-        return null;
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.save(item);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return item;
     }
 
     @Override
     public boolean replace(int id, Item item) {
-        return false;
+        boolean success = false;
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            Item existingItem = session.get(Item.class, id);
+
+            existingItem.setName(item.getName());
+            existingItem.setCreated(item.getCreated());
+
+            session.update(existingItem);
+            session.getTransaction().commit();
+            success = true;
+
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return success;
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        boolean result = false;
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery("DELETE from Item where id = :fid ")
+                    .setParameter("fid", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+            result = true;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return result;
     }
 
     @Override
     public List<Item> findAll() {
-        return List.of();
+        Session session = sf.openSession();
+        List<Item> result = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            result = session.createQuery("from Item", Item.class).list();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return result;
     }
 
     @Override
     public List<Item> findByName(String key) {
-        return List.of();
+        Session session = sf.openSession();
+        List<Item> result = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            result = session.createQuery("from Item where name = :fkey", Item.class)
+                    .setParameter("fkey", key)
+                    .list();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return result;
     }
 
     @Override
     public Item findById(int id) {
-        return null;
+        Session session = sf.openSession();
+        Item result = new Item();
+        try {
+            session.beginTransaction();
+            result = session.get(Item.class, id);
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return result;
     }
 }
